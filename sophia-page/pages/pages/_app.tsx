b49a5625aa806a79aee90881bd0d46d58120f3e5
swq2101 <69648780+swq2101@users.noreply.github.com>
2021-08-27 17:14:12 -0400

    Delete sophia-page directory

--- sophia-page/pages/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import 'tailwindcss/tailwind.css'
-import type { AppProps } from 'next/app'
-import { appWithTranslation } from "next-i18next";
-
-
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
-export default appWithTranslation(MyApp);
