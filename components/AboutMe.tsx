81a8d3bbe674210da3e211ec0706e20e179b6a69
swq2101 <69648780+swq2101@users.noreply.github.com>
2021-08-27 17:15:29 -0400

    Add files via upload

---
+++ components/AboutMe.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <section className="bg-white dark:bg-gray-800">
+      <div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
+        <h1 className=" text-5xl text-green-900 md:text-9xl font-bold py-20 text-center md:text-left">
+          About Me.
+        </h1>
+      </div>
+      <div className="bg-[#F1F1F1] -mt-10 dark:bg-gray-900">
+        <div className="text-container max-w-6xl mx-auto pt-20">
+          <p
+            className="leading-loose text-2xl md:text-4xl font-semibold  mx-4"
+            style={{ lineHeight: "3rem" }}
+          >
+            I'm a junior at Columbia University studying computer science and French. I started coding becuase I love spending time
+            on a screen and want to make that my job. 
+          </p>
+          <p
+            className="leading-loose text-lg md:text-lg  mx-4"
+            style={{ lineHeight: "3rem" }}
+          >
+            Right now, I'm looking for opporunities in product managment and AI/ML. Ideas and products that 
+            I'm specifically excited by are those pertaining to language/linguistics, media, fashion, food, or any industry 
+            with opportunity for novel development and growth!
+          </p>
+        </div>
+      </div>
+      <div className="bg-[#F1F1F1] dark:bg-gray-900 px-4">
+        <div className="pt-20 grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-y-20 gap-x-20">
+          {/* Social Buttons */}
+          <div className="inline-flex flex-col">
+            <div>
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Contact
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                Send me an {" "}
+                <a
+                  href={`mailto:qureshisophia2@gmail.com`}
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                >
+                  email
+                </a>{" "}
+                to get in touch!
+              </p>
+            </div>
+            <div className="mt-8">
+              <h1 className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+                Job Opportunities
+              </h1>
+              <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
+                If you think I might be a good fit for an opportunity, check out my {" "}
+                <a
+                  href={"https://docs.google.com/document/d/e/2PACX-1vQWceGb5wP7JQVdTp_qd35AH-UYChVn1NEfC4NF7IalgUr6ruVS_u4qoeS3iAaxXw/pub"}
+                  target="__blank"
+                  className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
+                >
+                  CV
+                </a>{" "}
+                and I'd love to work with you.
+              </p>
+            </div>
+            {/* Social Links */}
+            <h1 className="text-xl font-semibold text-gray-700 mt-8 dark:text-gray-200">
+              Social Links
+            </h1>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={"https://github.com/swq2101"}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    GitHub
+                  </p>
+                </a>
+              </div>
+              <div className="flex flex-row justify-start items-center">
+                <a
+                  href={"https://www.linkedin.com/in/sophia-q-40b180111/"}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    LinkedIn
+                  </p>
+                </a>
+              </div>
+          </div>
+          {/* Text area */}
+
+          </div>
+        </div>
+    </section>
+  );
+}
---
+++ components/ContainerBlock.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Navbar from "./Navbar";
+
+export default function ContainerBlock({ children, ...customMeta }) {
+  const router = useRouter();
+
+  return (
+    <div>
+      <Head>
+        <title>Sophia Qureshi</title>
+      </Head>
+      <main className="dark:bg-gray-800 w-full">
+        <Navbar />
+        <div>{children}</div>
+      </main>
+    </div>
+  );
+}
